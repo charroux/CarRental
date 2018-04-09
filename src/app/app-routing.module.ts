b321fc55c802b533ff7df1150ae0523a8fd9a517
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-04-09 11:42:02 +0200

    delete getbackcar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CarsComponent } from './cars.component';
 import { CarDetailComponent } from './car-detail.component';
-import { GetBackCarsComponent } from './getbackcars.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/cars', pathMatch: 'full' },
   { path: 'detail/:plateNumber', component: CarDetailComponent },
-  { path: 'cars',     component: CarsComponent },
-  { path: 'getbackcars',     component: GetBackCarsComponent }
+  { path: 'cars',     component: CarsComponent }
 ];
 
 @NgModule({
